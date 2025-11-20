b17edd645a37f988f4855d28554bb0bdee175387
Mahima <mahima.sharu@hashicorp.com>
2025-11-20 14:12:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 variable "enable" {
   description = "Toggle resource on/off. Set to true to create, false to destroy."
   type        = bool
-  default     = false
+  default     = true
 }
 
 resource "random_pet" "toggle" {
@@ -24,6 +24,15 @@ resource "random_pet" "toggle" {
   length = 7
 }
 
+resource "random_pet" "toggle1" {
+  count  = var.enable ? 1 : 0
+  length = 10
+}
+
 output "created_id" {
   value = length(random_pet.toggle) > 0 ? random_pet.toggle[0].id : "resource absent"
+}
+
+output "created_id_1" {
+  value = length(random_pet.toggle1) > 0 ? random_pet.toggle1[0].id : "resource absent"
 }
